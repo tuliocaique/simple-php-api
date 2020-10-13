143ba72edf8c6d833fdb951ff963d1932e096952
Tulio Caique <tulio.caique@gmail.com>
2020-10-13 15:12:16 -0300

    Create ModelClient.php

---
+++ modal/ModelClient.php
@@ -0,0 +1,104 @@
+<?php
+
+
+class ModelClient
+{
+    private string $id;
+    private string $name;
+    private string $email;
+    private string $password;
+    private string $country;
+
+    /**
+     * ModelUser constructor.
+     */
+    public function __construct()
+    {
+        $this->id = uniqid();
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+
+
+}
